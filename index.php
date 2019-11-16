6996c95c2f186fec948942b7c72301c7e19bf09e
Matheus Sampaio <matheusballestra@hotmail.com>
2019-11-15 21:12:59 -0300

    login system

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php include("cabecalho.php"); ?>
-            <h1>Bem vindo!</h1>
+
+<?php
+    if(isset($_GET["login"]) && $_GET["login"]==true) {
+?>
+        <p class="alert-success">Logado com sucesso!</p>
+<?php
+    }
+?>
+<?php
+    if(isset($_GET["login"]) && $_GET["login"]==false) {
+?>
+        <p class="alert-danger">Usuário ou senha inválida!</p>
+<?php
+    }  
+?>
+
+
+    <form action="login.php" method="post">
+        <table class="table">
+            <tr>
+                <td>Email</td>
+                <td><input type="text" class="form-control" name="email" /></td>
+            </tr>
+            <tr>
+                <td>Senha</td>
+                <td><input type="password" class="form-control" name="senha"></td>
+            </tr>
+            <tr>
+                <td><button type="submit" value="Cadastrar" class="btn btn-primary">submit</button></td>
+            </tr>
+        </table>
+    </form>
+
+
+
 <?php include("rodape.php"); ?>
+
